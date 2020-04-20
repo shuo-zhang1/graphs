e216e17ed4f67b67868e59215a809b9808e21620
shuo-zhang1 <shuozg@gmail.com>
2020-04-20 18:29:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -44,12 +44,7 @@ Graph createLinkedList(int n)
 
     for (int n = 0; n < graphNodes.size(); n++)
     {
-        curr = graphNodes[n];
-        int v1 = rand() % 100;
-        if(v1 < 20)
-        {
-            graph.addUndirectedEdge(curr, graphNodes[n + 1]);
-        }
+        graph.addUndirectedEdge(curr, graphNodes[n + 1]);
     }
     return graph;
 }
@@ -101,41 +96,5 @@ void BFTIterLinkedList(Graph graph)
 
 int main()
 {
-    Graph gsmall = createRandomUnweightedGraphIter(10);
-    Graph gbig = createLinkedList(20);
-    vector<Node *> allNodes1 = gsmall.getAllNodes();
-    print(allNodes1);
-    vector<Node *> allNodes2 = gbig.getAllNodes();
-    print(allNodes2);
-
-    Node *first = new Node("8");
-    Node *second = new Node("2");
-
-    gsmall.removeUndirectedEdge(first, second);
-    allNodes1 = gsmall.getAllNodes();
-    cout << "Removing edge between 2 and 8" << endl;
-    print(allNodes1);
-    GraphSearch search;
-    Node *target = new Node("9");
-    vector<Node *> DFSOutput;
-    DFSOutput = search.DFSRec(gsmall, allNodes1[0], target);
-    cout << "DFSRec from 0 to 9" << endl;
-    printTraversal(DFSOutput);
-    DFSOutput = search.DFSIter(gsmall, allNodes1[0], target);
-    cout << "DFSIter from 0 to 9" << endl;
-    printTraversal(DFSOutput);
-    vector<Node *> BFTOutput;
-    BFTOutput = search.BFTRec(gsmall);
-    cout << "BFTRec" << endl;
-    printTraversal(BFTOutput);
-
-    BFTOutput = search.BFTIter(gsmall);
-    cout << "BFTIter" << endl;
-    printTraversal(BFTOutput);
-
-    Graph tenk = createLinkedList(10000);
-    BFTRecLinkedList(tenk);
-    BFTIterLinkedList(tenk);
-
     return 0;
 }
